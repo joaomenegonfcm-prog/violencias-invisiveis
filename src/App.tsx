d15e2b3fd73172e4268d5989861df2665f9af208
João Menegon <joaomenegonfcm@gmail.com>
2026-08-12 11:11:41 -0300

    chore: setup inicial do projeto

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-dusk-950">
+      <h1 className="font-display text-4xl text-mist-100">
+        Violências Invisíveis
+      </h1>
+    </div>
+  )
+}
+
+export default App
